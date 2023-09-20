3a033041553367d26b499ba1b0f4ea5c09fcabe4
Dbp2401 <dbp24012003@gmail.com>
2023-09-20 15:44:01 +0200

    EJ1 Uploaded

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //Ej 1
+        System.out.println("PRIMER PROGRAMA");
+        //Circunferencia 2 * pi * r
+        //Area : pi * r^2 (pi * r * r)
+
+        double radio, circunferencia, area;
+        radio = 1.0;
+
+        circunferencia = 2*Math.PI*radio;
+        area = Math.PI * Math.pow(radio,2);
+
+
+        System.out.println("Area: " + area);
+
+        System.out.println("Circunferencia: " + circunferencia);
     }
 }
