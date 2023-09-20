cd4e0f3ed7acf9d97086751ca62f041fecfccd65
Dbp2401 <dbp24012003@gmail.com>
2023-09-20 15:45:56 +0200

    EJ2 Uploaded

--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
+    //Dependiente del segundo programa
+    public static boolean comprobarPrimo(int a){
+        for(int i=2;i<a-1;i++){
+            if(a%i==0) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         //Ej 1
         System.out.println("PRIMER PROGRAMA");
@@ -15,5 +26,23 @@ public class Main {
         System.out.println("Area: " + area);
 
         System.out.println("Circunferencia: " + circunferencia);
+
+
+        //Ej 2
+        //suma de primos
+        Scanner sc = new Scanner(System.in);
+        int a=4, b=4, c;
+        System.out.println("SEGUNDO PROGRAMA");
+        System.out.println("Elige dos numeros primos para sumarse");
+        while(comprobarPrimo(a)==false) {
+            System.out.print("Elige el primero: ");
+            a = sc.nextInt();
+        }
+        while(comprobarPrimo(b)==false) {
+            System.out.print("Elige el segundo: ");
+            b = sc.nextInt();
+        }
+        c=a+b;
+        System.out.println("Resultado de suma: "+ c);
     }
 }
